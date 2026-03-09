d184b5ee90226a9d7327ef610cd1efb2a20c8a3b
Yeonghun Ki <yeonghun.ki@gmail.com>
2026-03-08 23:41:09 -0700

    Update seed.sql to remove Google News and add JTBC, Donga

--- seed.sql
+++ seed.sql
@@ -3,4 +3,5 @@ INSERT OR IGNORE INTO sources (type, url, name) VALUES
   ('rss', 'https://www.chosun.com/arc/outboundfeeds/rss/?outputType=xml', '조선일보'),
   ('rss', 'https://www.khan.co.kr/rss/rssdata/total_news.xml', '경향신문'),
   ('rss', 'https://www.yna.co.kr/rss/news.xml', '연합뉴스'),
-  ('rss', 'https://news.google.com/rss?hl=ko&gl=KR&ceid=KR:ko', 'Google News');
+  ('rss', 'https://news-ex.jtbc.co.kr/v1/get/rss/newsflesh', 'JTBC'),
+  ('rss', 'https://rss.donga.com/total.xml', '동아일보');
